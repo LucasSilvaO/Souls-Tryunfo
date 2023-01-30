ef8e40d6f7ca510aaeddb9faa1008aa213f89e92
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2023-01-30 16:37:05 -0300

    Realiza requisito 5

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     cardImage: '',
     cardRare: '',
     cardTrunfo: '',
-    isSaveButtonDisabled: false,
+    isSaveButtonDisabled: true,
   };
 
   handleChange = ({ target }) => {
@@ -20,7 +20,63 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.handleUndefinedCases);
+  };
+
+  handleUndefinedCases = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage } = this.state;
+
+    const undefinedCases = [
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+    ];
+    const formularioPreenchido = undefinedCases.every((caso) => caso.length > 0);
+    console.log(formularioPreenchido);
+
+    if (formularioPreenchido && this.verificaParametros()) {
+      console.log('entrou');
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
+  };
+
+  verificaParametros = () => {
+    const {
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 } = this.state;
+
+    const atr1 = parseInt(cardAttr1, 10);
+    const atr2 = parseInt(cardAttr2, 10);
+    const atr3 = parseInt(cardAttr3, 10);
+    const Ttl = atr1 + atr2 + atr3;
+
+    const atributos = [
+      atr1,
+      atr2,
+      atr3];
+    const maxAtr = 90;
+    const maxTotal = 210;
+    const validateAtr = atributos.every((atributo) => atributo <= maxAtr);
+
+    if (validateAtr && Ttl <= maxTotal && atr1 >= 0 && atr2 >= 0 && atr3 >= 0) {
+      return true;
+    }
   };
 
   render() {
